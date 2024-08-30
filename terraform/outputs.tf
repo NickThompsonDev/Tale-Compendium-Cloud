f1af835e4820f42a26a7deb0e8ad91a783610e82
Nick Thompson <nickbdt86@gmail.com>
2024-08-30 02:21:19 -0400

    trying on a fresh state

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,16 @@
+# outputs.tf
+
 output "webapp_service_ip" {
   description = "The external IP of the webapp service"
-  value       = kubernetes_service.webapp.status[0].load_balancer[0].ingress[0].ip
+  value       = try(kubernetes_service.webapp.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
 }
 
 output "api_service_ip" {
   description = "The IP of the API service"
-  value       = kubernetes_service.api.status[0].load_balancer[0].ingress[0].ip
+  value       = try(kubernetes_service.api.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
 }
 
 output "database_service_ip" {
   description = "The IP of the database service"
-  value       = kubernetes_service.database.status[0].load_balancer[0].ingress[0].ip
+  value       = try(kubernetes_service.database.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
 }
