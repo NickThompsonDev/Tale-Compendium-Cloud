36609f9d22c63131241aa29dcd49d8ee7cce6dac
Nick Thompson <nickbdt86@gmail.com>
2024-09-05 05:14:24 -0400

    update outputs.tf to reference datasource

--- terraform/setup/outputs.tf
+++ terraform/setup/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
   description = "The name of the GKE cluster"
-  value       = google_container_cluster.primary.name
+  value       = data.google_container_cluster.primary.name
 }
 
 output "cluster_endpoint" {
-  description = "The GKE cluster endpoint"
-  value       = google_container_cluster.primary.endpoint
+  description = "The endpoint of the GKE cluster"
+  value       = data.google_container_cluster.primary.endpoint
 }
 
 output "ingress_namespace" {
