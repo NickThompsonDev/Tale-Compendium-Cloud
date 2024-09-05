a035e9a6e974f36c19835ae43d02704247c81b5e
Nick Thompson <nickbdt86@gmail.com>
2024-09-05 08:33:25 -0400

    terraform init, removed nested terraform since helm is not tf

--- terraform/setup/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_name" {
-  description = "The name of the GKE cluster"
-  value       = data.google_container_cluster.primary.name
-}
-
-output "cluster_endpoint" {
-  description = "The endpoint of the GKE cluster"
-  value       = data.google_container_cluster.primary.endpoint
-}
