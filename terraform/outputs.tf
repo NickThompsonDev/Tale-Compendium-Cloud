48a5d7b29772e82aa166d00601e205c08b6c6d49
Nick Thompson <nickbdt86@gmail.com>
2024-09-05 23:35:16 -0400

    undeclared service resources removed.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
 # outputs.tf
 
-output "webapp_service_ip" {
-  description = "The external IP of the webapp service"
-  value       = try(kubernetes_service.webapp.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
-}
+# output "webapp_service_ip" {
+#   description = "The external IP of the webapp service"
+#   value       = try(kubernetes_service.webapp.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
+# }
 
-output "api_service_ip" {
-  description = "The IP of the API service"
-  value       = try(kubernetes_service.api.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
-}
+# output "api_service_ip" {
+#   description = "The IP of the API service"
+#   value       = try(kubernetes_service.api.status[0].load_balancer[0].ingress[0].ip, "Pending IP allocation")
+# }
 
 output "database_service_ip" {
   description = "The IP of the database service"
