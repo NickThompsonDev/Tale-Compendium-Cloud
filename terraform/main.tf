b94b7eb3558853b92a8646c2ff4eb3dae3ba98be
Nick Thompson <nickbdt86@gmail.com>
2024-09-09 06:41:47 -0400

    temp destroy webapp_cert

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,12 @@ provider "helm" {
 }
 
 # 1. Create the Google-managed SSL Certificate
-resource "google_compute_managed_ssl_certificate" "webapp_cert" {
-  name = "webapp-managed-cert"
-  managed {
-    domains = ["cloud.talecompendium.com"]
-  }
-}
+# resource "google_compute_managed_ssl_certificate" "webapp_cert" {
+#   name = "webapp-managed-cert"
+#   managed {
+#     domains = ["cloud.talecompendium.com"]
+#   }
+# }
 
 # 2. Install NGINX Ingress Controller with Helm
 resource "helm_release" "nginx_ingress" {
